2a8b56d7f7af57346491e961db4ad40f71438368
ProfJust <olaf.just@w-hs.de>
2024-11-26 16:17:17 +0100

    pyqt6

--- Py_Qt6/pyQt6_12_mouse_event.py
+++ Py_Qt6/pyQt6_12_mouse_event.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-#pyqt_sw23_MouseEvent.py
+# pyQt6_12_mouse_event.py
+#---------------------------------------
+# Erstellt ein Fenster einem bewegten Streichholz
+# -----------------------------------------
+# last edited by OJ am 26.11.2024 
+#
+# Usage: 
+# in der Windows-Power-Shell in das VErzeichnis springen und
+# > cd 'C:\Users\RoboAdmin\mySciebo\_SRO\_GitHub_LaborPC\SRO\Py_Qt6\'
+# ausführen
+# > python .\pyQt6_12_mouse_event.py
+#
+# Hier aus dem Debugger funktioniert es nicht (Fenster ohne alles)
+# Die Bilddateie muss im selben Ordner liegen !!
 
-####### Noch nicht vollständig auf Qt6 umgestellt #################
-###################################################################
+# https://www.pythonguis.com/tutorials/pyqt6-signals-slots-events/#mouse-events
+
 
 import sys
 from PyQt6.QtCore import (Qt, QTimer, QRect)
 from PyQt6.QtWidgets import (QWidget, QPushButton, QApplication, QLabel)
 from PyQt6.QtGui import QPainter, QColor, QFont
-from PyQt6.QtGui import QPixmap, QKeyEvent
+from PyQt6.QtGui import QPixmap, QKeyEvent, QMouseEvent
 
 class Ui(QWidget):
     #statische Klassenvariablen
@@ -28,24 +40,18 @@ class Ui(QWidget):
         self.setMouseTracking(True)
         self.show()
     
-    def mouseMoveEvent(self, event): #Methode der QWidget-Klasse
-        self.mouse_pos_x = event.x()
-        self.mouse_pos_y = event.y()
-        #print Mouse Position
-        print('x: %d  y: %d' % (self.mouse_pos_x, self.mouse_pos_y))
-        
     def mousePressEvent(self, event): #Methode der QWidget-Klasse
-        if  event.button()== Qt.LeftButton:
+        if  event.button()== Qt.MouseButton.LeftButton:
             print('Linksklick')
-        if  event.button()== Qt.RightButton:
+        if  event.button()== Qt.MouseButton.RightButton:
             print("Rechtsklick")
             
     def mouseDoubleClickEvent(self, event): #Methode der QWidget-Klasse
-        if  event.button()== Qt.LeftButton:
+        if  event.button()== Qt.MouseButton.LeftButton:
             print('Linksklick doppelt, mit Rechtsklick doppelt wird es wieder klein')
             #self.showMaximized() #mit Titelzeile
             self.showFullScreen() #ohne Titelzeile
-        if  event.button()== Qt.RightButton:
+        if  event.button()== Qt.MouseButton.RightButton:
             print("Rechtsklick doppelt")
             self.showNormal() #urspruengiche Groesse
             #self.showMinimized() #ganz weg
@@ -56,16 +62,23 @@ class Ui(QWidget):
         self.drawFunc(event, p)        
         p.end()
     
+    def mouseMoveEvent(self, event): #Methode der QWidget-Klasse
+        self.mouse_pos_x = event.position().x()
+        self.mouse_pos_y = event.position().y()
+        #print Mouse Position
+        print('x: %d  y: %d' % (self.mouse_pos_x, self.mouse_pos_y))
+
     def keyPressEvent(self, event): #Methode der QWidget-Klasse
-        if event.key() == Qt.Key_Left:
+        #if e.key() == Qt.Key.Key_Escape.value:
+        if event.key() == Qt.Key.Key_Left:
             self.keyLeft = True
-        if event.key() == Qt.Key_Right:
+        if event.key() == Qt.Key.Key_Right:
             self.keyRight = True
             
     def keyReleaseEvent(self, event): #Methode der QWidget-Klasse
-        if event.key() == Qt.Key_Left:
+        if event.key() == Qt.Key.Key_Left:
              self.keyLeft = False
-        if event.key() == Qt.Key_Right:
+        if event.key() == Qt.Key.Key_Right:
              self.keyRight = False
         event.accept()
     
