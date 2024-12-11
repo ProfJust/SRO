edc82135fc2fe329cc66680f19f0ba6e0b81c8a8
ProfJust <olaf.just@w-hs.de>
2024-12-11 18:10:36 +0100

    tested

--- SRO_RTDE_mit_Gripper/rtde_montage_test.py
+++ SRO_RTDE_mit_Gripper/rtde_montage_test.py
@@ -2,14 +2,16 @@
 #----------------------------------------
 # Montage eines Lego-Duplo-Turms
 # ---------------------------------------
-# last edited by OJ 6.12.24
+# last edited by OJ 11.12.24
 # to be Tested 4 on UR5e
 # Basierend auf dem Force-Mode-Exampe auf
 # https://sdurobotics.gitlab.io/ur_rtde/examples/examples.html#forcemode-example
-
+#
+# ggf. aus dem Administrator-Terminal starten
 
 import rtde_control      # Steuerungsfunktionen des Roboters über RTDE
-ROBOT_IP = "127.0.0.1"   # IP des Roboters
+import math
+ROBOT_IP = "192.168.0.3"   # IP des Roboters hier UR5e
 rtde_c = rtde_control.RTDEControlInterface(ROBOT_IP)  # Verbindung herstellen
 
 # Bezugsrahmen für die Kraftsteuerung setzen, in diesem Fall kein Offset (ein Null-Vektor).
@@ -20,8 +22,8 @@ task_frame = [0, 0, 0, 0, 0, 0]  #  Aufgabenspezifische Koordinatensystem
 selection_vector = [0, 0, 1, 0, 0, 0]  
 
 #  Diese Vektoren für den forceMode definieren die Kräfte/Momente,des Roboters
-wrench_down = [0, 0, -10, 0, 0, 0]  # Abwärtskraft von `-10 N` entlang der Z-Achse
-wrench_up   = [0, 0,  10, 0, 0, 0]  # Aufwärtskraft von `10 N` entlang der Z-Achse
+wrench_down = [0, 0, -1, 0, 0, 0]  # Abwärtskraft von `-10 N` entlang der Z-Achse
+wrench_up   = [0, 0,  1, 0, 0, 0]  # Aufwärtskraft von `10 N` entlang der Z-Achse
 
 # Modus der Kraftregelung: `2` steht häufig für eine Kraftsteuerung auf Basis von Kräften, die auf den Endeffektor wirken.
 # 0 - Krafteinstellung linearer Achsen (Translation):
@@ -39,9 +41,23 @@ limits = [2, 2, 1.5, 1, 1, 1]
 dt = 1.0/500  # 2ms
 
 # Anfangsposition der Gelenke des Roboters.
-joint_q = [-1.54, -1.83, -2.28, -0.59, 1.60, 0.023]
+joint_q_deg = [-29.85, -102.66, 139.09, -130.74, -89.54, -28.62] # vom Teachpendant abgelesen
+joint_q_rad =[] # Instanziere leere Liste
+# rtde_r.getActualTCPPose()
+for deg in joint_q_deg:
+    #print(deg)
+    rad = deg /180.0 * math.pi
+    #print(rad)
+    joint_q_rad.append(rad)
+
+print(joint_q_deg)
+print(joint_q_rad)    
+
+#joint_q = [-1.54, -1.83, -2.28, -0.59, 1.60, 0.023]
+
+input("Move Robot?")
 # Move to initial joint position with a regular moveJ
-rtde_c.moveJ(joint_q)
+rtde_c.moveJ(joint_q_rad)
 
 # Execute 500Hz realtime control loop for 4 seconds, each cycle is 2ms
 # -----------------------------------------------------------------------------
@@ -50,19 +66,25 @@ rtde_c.moveJ(joint_q)
 # In Python erzeugt die Funktion `range(2000)` eine Sequenz von Zahlen, die bei 0 beginnt und bis zur Zahl 1999 reicht. 
 # Die Schleife `for i in range(2000):` bedeutet damit, dass die Schleife 2000 Mal ausgeführt wird, 
 # wobei `i` in jeder Iteration auf den nächsten ganzzahligen Wert in der Sequenz gesetzt wird. 
-for i in range(2000):  # Schleife 2000mal ausführen i=0...1999
-    # Zeitmessung initialisieren zur Überwachung der aktuellen Steuerperiode (hier 2 ms)
-    t_start = rtde_c.initPeriod() # holt aktuelle Systemzeit / Zeitstempel
-    # First move the robot down for 2 seconds, then up for 2 seconds
-    if i > 1000:
-        rtde_c.forceMode(task_frame, selection_vector, wrench_up, force_type, limits)
-    else:
-        rtde_c.forceMode(task_frame, selection_vector, wrench_down, force_type, limits)
+try:
+    for i in range(2000):  # Schleife 2000mal ausführen i=0...1999
+        # Zeitmessung initialisieren zur Überwachung der aktuellen Steuerperiode (hier 2 ms)
+        t_start = rtde_c.initPeriod() # holt aktuelle Systemzeit / Zeitstempel
+        # First move the robot down for 2 seconds, then up for 2 seconds
+        if i > 1000:
+            rtde_c.forceMode(task_frame, selection_vector, wrench_up, force_type, limits)
+        else:
+            rtde_c.forceMode(task_frame, selection_vector, wrench_down, force_type, limits)
 
-    # Indem du `initPeriod()` am Anfang einer Steuerperiode und `waitPeriod(t_start)` am Ende aufrufst, 
-    # kannst du die Ausführung so koordinieren, dass der Steuerungs-Loop mit konstanter Frequenz, hier genau alle 2 ms, ausgeführt wird.    
-    rtde_c.waitPeriod(t_start) # Wartet auf Ablauf der Zeit t_start + dt 
+        # Indem du `initPeriod()` am Anfang einer Steuerperiode und `waitPeriod(t_start)` am Ende aufrufst, 
+        # kannst du die Ausführung so koordinieren, dass der Steuerungs-Loop mit konstanter Frequenz, hier genau alle 2 ms, ausgeführt wird.    
+        rtde_c.waitPeriod(t_start) # Wartet auf Ablauf der Zeit t_start + dt 
 
-# Zum Schluss wird der Kraftmodus beendet, und das Skript wird gestoppt, wodurch die Ausführung beendet wird.
-rtde_c.forceModeStop()
-rtde_c.stopScript()
+    # Zum Schluss wird der Kraftmodus beendet, und das Skript wird gestoppt, wodurch die Ausführung beendet wird.
+    rtde_c.forceModeStop()
+    rtde_c.stopScript()
+    print("\n Ende \n")
+
+except KeyboardInterrupt:
+    rtde_c.stopScript()
+
