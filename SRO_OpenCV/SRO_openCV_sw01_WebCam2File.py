c21a665b11ae6a5d68aa5e064f8cf2e5b14450b3
ProfJust <olaf.just@w-hs.de>
2025-11-05 16:19:21 +0100

    CAMERA_INDEX, cv.CAP_DSHOW

--- SRO_OpenCV/SRO_openCV_sw01_WebCam2File.py
+++ SRO_OpenCV/SRO_openCV_sw01_WebCam2File.py
@@ -5,16 +5,22 @@
 # ggf. pip install opencv-python
 import cv2 as cv
 
+
+print("Lese Bild von Kamera und speichere als Datei ")
 # initialisiere WebCam
-cam = cv.VideoCapture(0)
+CAMERA_INDEX = 0
+cam = cv.VideoCapture(CAMERA_INDEX, cv.CAP_DSHOW) 
+# cv.CAP_DSHOW => dauert nicht so lange bis Bild von USB-Kamera kommt
+print("Kamera initialisiert")
 
 # WebCam braucht einen Moment zum Starten
 # und zum Einstellen des Autofokus
 # => ggf. mehrere Bilder holen und die ersten verwerfen
 
+
 # lese ein Bild von der WebCam
-# ret, image = cam.read()
-# ret, image = cam.read()
+ret, dummy = cam.read()
+ret, dummy = cam.read()
 ret, img = cam.read()
 
 # zeige das Bild an
--- SRO_OpenCV/SRO_openCV_sw01_firstTest.py
+++ SRO_OpenCV/SRO_openCV_sw01_firstTest.py
@@ -1,26 +1,34 @@
 # SRO_openCV_sw01_firstTest.py
 # ................................
-# Tested by OJ am 15.11.24 
+# Tested by OJ am 9.5.25 
 # python 3.12.7
+# WebCam anschliessen
 #-------------------------------------
 # ggf. python.exe -m pip install --upgrade pip
 # ggf. pip install opencv-python
 import cv2 as cv
 
 # initialisiere WebCam
-cam = cv.VideoCapture(0)
+CAMERA_INDEX = 0
+cam = cv.VideoCapture(CAMERA_INDEX, cv.CAP_DSHOW) 
+# cv.CAP_DSHOW => dauert nicht so lange bis Bild von USB-Kamera kommt
+
 
 # WebCam braucht einen Moment zum Starten
 # und zum Einstellen des Autofokus
 # => ggf. mehrere Bilder holen und die ersten verwerfen
 
 # lese ein Bild von der WebCam
-# ret, image = cam.read()
-# ret, image = cam.read()
+ret, image = cam.read()
+ret, image = cam.read()
+
+
 ret, image = cam.read()
 
 # zeige das Bild an
+print("Lese Bild von Kamera und zeige es an ")
 cv.imshow("WebCam", image)
+print("Kamera initialisiert, in Fenster klicken und Taste drücken")
 cv.waitKey(0)
 
 # konvertiere das Bild in Graustufen
